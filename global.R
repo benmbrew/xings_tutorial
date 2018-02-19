d83df0da66a6149b8b10498936845f826a47bd4e
benmbrew <benmbrew@gmail.com>
2018-02-19 14:26:41 -0500

    global created with data read in

--- global.R
+++ global.R
@@ -1,1 +1,9 @@
-#####
+##### This script wil read in the three YWCA data sets and get them in a format that is app ready
+library(tidyverse)
+library(readr)
+
+# readin in data 
+hr_data <- read_csv('data/hr_stats.csv')
+time_data <- read_csv('data/stats_over_time.csv')
+total_data <- read_csv('data/total_stats.csv')
+
