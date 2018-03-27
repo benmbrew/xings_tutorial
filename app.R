0f7d6da9b63e5b6a283e8bdb5ec23ca8a0716169
benmbrew <benmbrew@gmail.com>
2018-03-27 18:22:06 -0400

    first app made

--- app.R
+++ app.R
@@ -3,101 +3,71 @@ library(shinydashboard)
 library(DT)
 
 
+source('global.R')
+
 ui <- dashboardPage(skin = 'red',
                     
                     
                     dashboardHeader(
-                      title = "LFS database at SickKids",
+                      title = "YWCA data dashboard",
                       titleWidth = 300
                     ),
                     
-                    dashboardSidebar(width = 300,
-                                     sidebarMenu(
-                                       menuItem('Raw data',
-                                                icon = icon('table'),
-                                                tabName = 'raw_data'),
-                                       menuItem('Summary stats',
-                                                icon = icon('bar-chart-o'),
-                                                tabName = 'ywca_stats'))),
+                    dashboardSidebar(disable = TRUE),
+                    
+                    
                     dashboardBody(
                       tags$head(
                         tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
                       ),
-                      tabItems(
-                        tabItem(
-                          tabName = "raw_data",
-                                h2('Download clinical data'),
-                                helpText(''),
-                                fluidRow(column(12,
-                                                DT::dataTableOutput('clin_table'))),
-                          tabsetPanel(
-                            tabPanel('HR data',
-                                     fluidRow(
-                                       column(12,
-                                              DT::dataTableOutput('hr_table'))
-                                     )
-                                     
-                            ),
-                            
-                            tabPanel('Program stats',
-                                     fluidRow(
-                                       column(12,
-                                              DT::dataTableOutput('program_table'))
-                                     )
-                                     
-                            ),
-                            tabPanel('Stats over time',
-                                     fluidRow(
-                                       column(12,
-                                              DT::dataTableOutput('over_time_table'))
-                                     )
-                            )
-
-                          )
-                                
-                        ),
-                        tabItem(
-                          tabName = "ywca_stats",
-                                h2('Explore YWCA data and stats'),
-                                helpText(''),
-                                fluidRow(column(4, 
-                                                checkboxInput('cancer_status',
-                                                              'Cancer type',
-                                                              value = NULL)),
-                                         column(4,
-                                                checkboxInput('p53_status',
-                                                              'tp53 status',
-                                                              value = NULL)),
-                                         column(4,
-                                                checkboxInput('gender',
-                                                              'Gender',
-                                                              value = NULL))),
-                                fluidRow(column(4,
-                                                uiOutput('cancer_filter')),
-                                         column(4,
-                                                uiOutput('p53_filter')),
-                                         column(4,
-                                                uiOutput('gender_filter'))),
-                                
-                                
-                                tabsetPanel(
-                                  tabPanel('Table',
-                                           fluidRow(column(12,
-                                                           textOutput('xing_text'),
-                                                           DT::dataTableOutput('xing_table')
-                                           ))),
-                                  tabPanel('Plot', 
-                                           plotOutput('bar_plot'))))
-                        
-                      )))
+                      
+                      fluidRow(column(12,
+                                      align = 'center',
+                                      selectInput('var_category',
+                                                  'Choose a variable',
+                                                  choices = unique(time_data$variable),
+                                                  selected = unique(time_data$variable)[1:5],
+                                                  multiple = TRUE))),
+                      fluidRow(column(6,
+                                      plotOutput('plot_cat')),
+                               column(6,
+                                      DT::dataTableOutput('table_cat')))
+                     
+           
+                   ))
 
 
 
 
 # Define server 
 server <- function(input, output) {
-  # gender filter
+  # gender filteru
 
+  output$plot_cat <- renderPlot({
+    
+    var_category <- input$var_category
+    
+    # subset data 
+    sub_data <- time_data %>% filter(variable %in% var_category)
+    
+    # ggplot line 
+    ggplot(sub_data, aes(year, value, color = variable)) +
+      geom_line()
+    
+  })
+  
+  output$table_cat <- renderDataTable({
+    
+    var_category <- input$var_category
+    
+    # subset data 
+    sub_data <- time_data %>% filter(variable %in% var_category)
+    
+    datatable(sub_data)
+    
+    
+  })
+  
   
 }
 
--- global.R
+++ global.R
@@ -1,9 +1,28 @@
 ##### This script wil read in the three YWCA data sets and get them in a format that is app ready
 library(tidyverse)
 library(readr)
+library(reshape2)
 
 # readin in data 
 hr_data <- read_csv('data/hr_stats.csv')
 time_data <- read_csv('data/stats_over_time.csv')
 total_data <- read_csv('data/total_stats.csv')
+
+
+# explore time data
+str(time_data)
+time_data <- as.data.frame(time_data)
+
+# make data long 
+time_data <- melt(time_data, id.vars = 'Category')
+
+# rename category to year
+names(time_data)[1] <- 'year'
+
+rm(hr_data, total_data)
+# save the new data
+
+save.image('data/time_data.RData' )
+
+
 
